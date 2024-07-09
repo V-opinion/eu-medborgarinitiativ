0748e8e8e0785de073e0eab87efc0b9938da0e3f
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-07-09 23:35:14 +0200

    Add files via upload

---
+++ oi/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_3_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ caption: "F.D.U",
+ image: "multireligionval_htm_files/430.jpg"
+},
+{
+ caption: "F.D.U",
+ image: "multireligionval_htm_files/536.jpg"
+},
+{
+ caption: "F.D.U\u000A..",
+ image: "multireligionval_htm_files/432.jpg"
+},
+{
+ caption: "F.D.U\u000A..",
+ image: "multireligionval_htm_files/433.jpg"
+}
+];
