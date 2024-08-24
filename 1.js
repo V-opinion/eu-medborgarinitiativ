dc15c9eb6a1b67828b57ff17a315e977a5f864b8
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-08-24 23:07:20 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,44 @@
+window.local_cyclemec_0_data = [
+{
+ effect: "5",
+ pause: "5",
+ speed: "0.7",
+ theme: "0",
+ trans: "0.8"
+},
+{
+ bimage: "index_htm_files/412.png",
+ desc: "index_htm_files/413.png",
+ heading: "index_htm_files/414.png",
+ image: "index_htm_files/415.png",
+ link: "#"
+},
+{
+ bimage: "index_htm_files/412.png",
+ desc: "index_htm_files/416.png",
+ heading: "index_htm_files/417.png",
+ image: "index_htm_files/418.jpg",
+ link: "#"
+},
+{
+ bimage: "index_htm_files/412.png",
+ desc: "index_htm_files/419.png",
+ heading: "index_htm_files/420.png",
+ image: "index_htm_files/421.png",
+ link: "#"
+},
+{
+ bimage: "index_htm_files/412.png",
+ desc: "index_htm_files/422.png",
+ heading: "index_htm_files/423.png",
+ image: "index_htm_files/424.png",
+ link: "#"
+},
+{
+ bimage: "index_htm_files/412.png",
+ desc: "index_htm_files/425.png",
+ heading: "index_htm_files/426.png",
+ image: "index_htm_files/427.jpg",
+ link: "#"
+}
+];
