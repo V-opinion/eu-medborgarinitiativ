3c9f7743c75697099a9125171a98a5d086d6644c
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-08-25 19:35:30 +0200

    Add files via upload

---
+++ svenska-kyrkans-privilegier/1.js
@@ -0,0 +1,44 @@
+window.local_cyclemec_10_data = [
+{
+ effect: "5",
+ pause: "5",
+ speed: "0.7",
+ theme: "0",
+ trans: "0.8"
+},
+{
+ bimage: "svenska-kyrkans-privilegier_htm_files/574.png",
+ desc: "svenska-kyrkans-privilegier_htm_files/575.png",
+ heading: "svenska-kyrkans-privilegier_htm_files/576.png",
+ image: "svenska-kyrkans-privilegier_htm_files/577.png",
+ link: "#"
+},
+{
+ bimage: "svenska-kyrkans-privilegier_htm_files/574.png",
+ desc: "svenska-kyrkans-privilegier_htm_files/578.png",
+ heading: "svenska-kyrkans-privilegier_htm_files/579.png",
+ image: "svenska-kyrkans-privilegier_htm_files/580.jpg",
+ link: "#"
+},
+{
+ bimage: "svenska-kyrkans-privilegier_htm_files/574.png",
+ desc: "svenska-kyrkans-privilegier_htm_files/581.png",
+ heading: "svenska-kyrkans-privilegier_htm_files/582.png",
+ image: "svenska-kyrkans-privilegier_htm_files/583.png",
+ link: "#"
+},
+{
+ bimage: "svenska-kyrkans-privilegier_htm_files/574.png",
+ desc: "svenska-kyrkans-privilegier_htm_files/584.png",
+ heading: "svenska-kyrkans-privilegier_htm_files/585.png",
+ image: "svenska-kyrkans-privilegier_htm_files/586.png",
+ link: "#"
+},
+{
+ bimage: "svenska-kyrkans-privilegier_htm_files/574.png",
+ desc: "svenska-kyrkans-privilegier_htm_files/587.png",
+ heading: "svenska-kyrkans-privilegier_htm_files/588.png",
+ image: "svenska-kyrkans-privilegier_htm_files/589.jpg",
+ link: "#"
+}
+];
