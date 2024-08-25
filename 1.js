ecfa0dd6599bb5e0485947c73e75f1a20ddf7ad4
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-08-25 15:34:30 +0200

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,44 @@
+window.local_cyclemec_0_data = [
+{
+ effect: "5",
+ pause: "5",
+ speed: "0.7",
+ theme: "0",
+ trans: "0.8"
+},
+{
+ bimage: "https://multireligionvalsystem.eu.org/412.png",
+ desc: "https://multireligionvalsystem.eu.org/413.png",
+ heading: "https://multireligionvalsystem.eu.org/414.png",
+ image: "https://multireligionvalsystem.eu.org/415.png",
+ link: "#"
+},
+{
+ bimage: "https://multireligionvalsystem.eu.org/412.png",
+ desc: "https://multireligionvalsystem.eu.org/416.png",
+ heading: "https://multireligionvalsystem.eu.org/417.png",
+ image: "https://multireligionvalsystem.eu.org/418.jpg",
+ link: "#"
+},
+{
+ bimage: "https://multireligionvalsystem.eu.org/412.png",
+ desc: "https://multireligionvalsystem.eu.org/419.png",
+ heading: "https://multireligionvalsystem.eu.org/420.png",
+ image: "https://multireligionvalsystem.eu.org/421.png",
+ link: "#"
+},
+{
+ bimage: "https://multireligionvalsystem.eu.org/412.png",
+ desc: "https://multireligionvalsystem.eu.org/422.png",
+ heading: "https://multireligionvalsystem.eu.org/423.png",
+ image: "https://multireligionvalsystem.eu.org/424.png",
+ link: "#"
+},
+{
+ bimage: "https://multireligionvalsystem.eu.org/412.png",
+ desc: "https://multireligionvalsystem.eu.org/425.png",
+ heading: "https://multireligionvalsystem.eu.org/426.png",
+ image: "https://multireligionvalsystem.eu.org/427.jpg",
+ link: "#"
+}
+];
