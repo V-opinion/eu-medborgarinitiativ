9c4e7c95397fd9571c305fcf5dfb316a5668f6e7
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-09-09 23:22:00 +0200

    Add files via upload

---
+++ riksmotet/1.js
@@ -0,0 +1,27 @@
+window.local_coinSlider_v8_0_data = [
+{
+ desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
+ image: "riksmotets-oppnande_htm_files/50.png",
+ link: "#"
+},
+{
+ desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar ",
+ image: "riksmotets-oppnande_htm_files/51.jpg",
+ link: "#"
+},
+{
+ desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
+ image: "riksmotets-oppnande_htm_files/52.png",
+ link: "#"
+},
+{
+ desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
+ image: "riksmotets-oppnande_htm_files/53.png",
+ link: "#"
+},
+{
+ desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
+ image: "riksmotets-oppnande_htm_files/54.png",
+ link: "#"
+}
+];
