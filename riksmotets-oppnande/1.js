8c488799ca3b536e2cf3bb5d78a7725eabbfb37a
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-09-09 22:46:10 +0200

    Update 1.js

--- riksmotets-oppnande/1.js
+++ riksmotets-oppnande/1.js
@@ -1,27 +1,27 @@
 window.local_coinSlider_v8_0_data = [
 {
  desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
- image: "riksmotets-oppnande_htm_files/24.png",
+ image: "https://multireligionvalsystem.eu.org/riksmotets-oppnande/24.png",
  link: "#"
 },
 {
  desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar ",
- image: "riksmotets-oppnande_htm_files/25.jpg",
+ image: "https://multireligionvalsystem.eu.org/riksmotets-oppnande/25.jpg",
  link: "#"
 },
 {
  desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
- image: "riksmotets-oppnande_htm_files/26.png",
+ image: "https://multireligionvalsystem.eu.org/riksmotets-oppnande/26.png",
  link: "#"
 },
 {
  desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
- image: "riksmotets-oppnande_htm_files/27.png",
+ image: "https://multireligionvalsystem.eu.org/riksmotets-oppnandes/27.png",
  link: "#"
 },
 {
  desc: "Genom att EU styrs av representanter f\u00F6r hungerregeringar \u00E4gnar ni er politik \u00E5t min SL\u00D6JA i st\u00E4llet f\u00F6r krig, milj\u00F6f\u00F6rst\u00F6relse , fattigdom , hj\u00E4lpa flyktingar och att f\u00E5 fungerande regeringar",
- image: "riksmotets-oppnande_htm_files/28.png",
+ image: "https://multireligionvalsystem.eu.org/riksmotets-oppnande/28.png",
  link: "#"
 }
 ];
