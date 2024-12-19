aa78c0852a895ff50fffd0b4afc019ce116b0f1d
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2024-12-19 20:47:52 +0100

    Add files via upload

---
+++ How-to-Implement-a-Multi-Religion-Electoral-System-A-Step-by-Step-Guide/1.js
@@ -0,0 +1,44 @@
+window.local_cyclemec_0_data = [
+{
+ effect: "5",
+ pause: "5",
+ speed: "0.7",
+ theme: "0",
+ trans: "0.8"
+},
+{
+ bimage: "slider_htm_files/729.png",
+ desc: "slider_htm_files/730.png",
+ heading: "slider_htm_files/731.png",
+ image: "slider_htm_files/732.png",
+ link: "#"
+},
+{
+ bimage: "slider_htm_files/729.png",
+ desc: "slider_htm_files/733.png",
+ heading: "slider_htm_files/734.png",
+ image: "slider_htm_files/735.jpg",
+ link: "#"
+},
+{
+ bimage: "slider_htm_files/729.png",
+ desc: "slider_htm_files/736.png",
+ heading: "slider_htm_files/737.png",
+ image: "slider_htm_files/738.png",
+ link: "#"
+},
+{
+ bimage: "slider_htm_files/729.png",
+ desc: "slider_htm_files/739.png",
+ heading: "slider_htm_files/740.png",
+ image: "slider_htm_files/741.jpg",
+ link: "#"
+},
+{
+ bimage: "slider_htm_files/729.png",
+ desc: "slider_htm_files/742.png",
+ heading: "slider_htm_files/743.png",
+ image: "slider_htm_files/744.jpg",
+ link: "#"
+}
+];
