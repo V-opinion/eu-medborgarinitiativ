e860e86a488b95c59ace39e91e27735ba04bd5e1
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-03-20 22:52:42 +0100

    Add files via upload

---
+++ Enskild-motion-FDU-Riksdagen/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_0_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ caption: "Den gr\u00F6na omst\u00E4llningen",
+ image: "cover_htm_files/19.jpg"
+},
+{
+ caption: "H\u00E5llbar utveckling.",
+ image: "cover_htm_files/55.png"
+},
+{
+ caption: "Den svenska deokratin..",
+ image: "cover_htm_files/56.jpg"
+},
+{
+ caption: "Multireligiovalsystem",
+ image: "cover_htm_files/57.jpg"
+}
+];
