bfb9ed49ddd455d041c88a0d295cba7b24635f59
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-03-23 00:23:08 +0100

    Add files via upload

---
+++ EU-utnyttjas-som-propaganda-for-NATO/1.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_0_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/420.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/421.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/422.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/423.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/424.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/425.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/426.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/427.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/428.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/429.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/430.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/431.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/432.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/433.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/434.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/435.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/436.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/437.jpg"
+}
+];
---
+++ EU-utnyttjas-som-propaganda-for-NATO/2.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_1_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/420.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/440.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/422.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/441.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/424.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/442.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/426.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/443.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/428.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/444.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/430.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/445.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/432.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/446.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/434.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/447.jpg"
+},
+{
+ image: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/436.jpg",
+ thumbnail: "EU-utnyttjas-som-propaganda-for-NATO_htm_files/448.jpg"
+}
+];
