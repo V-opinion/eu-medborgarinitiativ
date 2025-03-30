45d50b91a25af9466f4e0ca8dafe6b20524b9735
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-03-30 20:20:00 +0200

    Add files via upload

---
+++ demokratin/den-odemokratiska-demokratiministern/1.js
@@ -0,0 +1,22 @@
+window.local_nivoslider_10_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ caption: "Den gr\u00F6na omst\u00E4llningen",
+ image: "den-odemokratiska-demokratiministern_htm_files/558.jpg"
+},
+{
+ caption: "H\u00E5llbar utveckling.",
+ image: "den-odemokratiska-demokratiministern_htm_files/559.png"
+},
+{
+ caption: "Den svenska deokratin..",
+ image: "den-odemokratiska-demokratiministern_htm_files/560.jpg"
+},
+{
+ caption: "Multireligiovalsystem",
+ image: "den-odemokratiska-demokratiministern_htm_files/561.jpg"
+}
+];
