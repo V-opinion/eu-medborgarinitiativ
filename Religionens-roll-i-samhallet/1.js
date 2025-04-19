34f7054baadd604182e463ab0bebdfc88deb6c9a
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-04-19 17:03:42 +0200

    Update 1.js

--- Religionens-roll-i-samhallet/1.js
+++ Religionens-roll-i-samhallet/1.js
@@ -1,27 +1,27 @@
 window.local_coinSliderNonav_v8_0_data = [
 {
  desc: "Krig utg\u00F6r ett hot mot milj\u00F6n och m\u00E4nskligheten.",
- image: "Religionens-roll-i-samhallet_htm_files/21.jpg",
+ image: "https://multireligionvalsystem.eu.org/Religionens-roll-i-samhallet/21.jpg",
  link: "#"
 },
 {
  desc: "Religionens roll i samh\u00E4llet ! ",
- image: "Religionens-roll-i-samhallet_htm_files/22.jpg",
+ image: "https://multireligionvalsystem.eu.org/Religionens-roll-i-samhallet/22.jpg",
  link: "#"
 },
 {
  desc: "Ad, sed reprehenderit excepteur mollit consectetur in sit dolore. Nulla aute ",
- image: "Religionens-roll-i-samhallet_htm_files/23.png",
+ image: "https://multireligionvalsystem.eu.org/Religionens-roll-i-samhallet/23.png",
  link: "#"
 },
 {
  desc: "De nordiska kyrkornas privilegier",
- image: "Religionens-roll-i-samhallet_htm_files/24.jpg",
+ image: "https://multireligionvalsystem.eu.org/Religionens-roll-i-samhallet/24.jpg",
  link: "#"
 },
 {
  desc: "Genom denna effektiva reform separerar vi staten fr\u00E5n kyrkan samt religionen fr\u00E5n politiken p\u00E5 riktigt f\u00F6r att inf\u00F6ra demokrati i alla trossamfund.. ",
- image: "Religionens-roll-i-samhallet_htm_files/25.jpg",
+ image: "https://multireligionvalsystem.eu.org/Religionens-roll-i-samhallet/25.jpg",
  link: "#"
 }
 ];
