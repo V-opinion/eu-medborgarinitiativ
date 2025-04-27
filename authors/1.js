09cff63280c4912dbda6bcbea69d9598a71b5d8e
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-04-27 17:57:08 +0200

    Add files via upload

---
+++ authors/1.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_0_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "author_htm_files/471.jpg",
+ thumbnail: "author_htm_files/472.jpg"
+},
+{
+ image: "author_htm_files/473.jpg",
+ thumbnail: "author_htm_files/474.jpg"
+},
+{
+ image: "author_htm_files/475.jpg",
+ thumbnail: "author_htm_files/476.jpg"
+},
+{
+ image: "author_htm_files/477.jpg",
+ thumbnail: "author_htm_files/478.jpg"
+},
+{
+ image: "author_htm_files/479.jpg",
+ thumbnail: "author_htm_files/480.jpg"
+},
+{
+ image: "author_htm_files/481.jpg",
+ thumbnail: "author_htm_files/482.jpg"
+},
+{
+ image: "author_htm_files/483.jpg",
+ thumbnail: "author_htm_files/484.jpg"
+},
+{
+ image: "author_htm_files/485.jpg",
+ thumbnail: "author_htm_files/486.jpg"
+},
+{
+ image: "author_htm_files/487.jpg",
+ thumbnail: "author_htm_files/488.jpg"
+}
+];
---
+++ authors/2.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_1_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "author_htm_files/471.jpg",
+ thumbnail: "author_htm_files/490.jpg"
+},
+{
+ image: "author_htm_files/473.jpg",
+ thumbnail: "author_htm_files/491.jpg"
+},
+{
+ image: "author_htm_files/475.jpg",
+ thumbnail: "author_htm_files/492.jpg"
+},
+{
+ image: "author_htm_files/477.jpg",
+ thumbnail: "author_htm_files/493.jpg"
+},
+{
+ image: "author_htm_files/479.jpg",
+ thumbnail: "author_htm_files/494.jpg"
+},
+{
+ image: "author_htm_files/481.jpg",
+ thumbnail: "author_htm_files/495.jpg"
+},
+{
+ image: "author_htm_files/483.jpg",
+ thumbnail: "author_htm_files/496.jpg"
+},
+{
+ image: "author_htm_files/485.jpg",
+ thumbnail: "author_htm_files/497.jpg"
+},
+{
+ image: "author_htm_files/487.jpg",
+ thumbnail: "author_htm_files/498.jpg"
+}
+];
