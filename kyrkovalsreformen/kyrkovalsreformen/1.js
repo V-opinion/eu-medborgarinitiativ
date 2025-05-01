2ae2e27198a382c140ae0a19d709a686a3079080
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-05-02 00:29:58 +0200

    Add files via upload

---
+++ kyrkovalsreformen/kyrkovalsreformen/1.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_0_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/493.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/494.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/495.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/496.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/497.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/498.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/499.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/500.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/501.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/502.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/503.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/504.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/505.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/506.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/507.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/508.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/509.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/510.jpg"
+}
+];
---
+++ kyrkovalsreformen/kyrkovalsreformen/2.js
@@ -0,0 +1,42 @@
+window.local_simpleGallery3Columns_1_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/493.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/513.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/495.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/514.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/497.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/515.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/499.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/516.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/501.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/517.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/503.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/518.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/505.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/519.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/507.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/520.jpg"
+},
+{
+ image: "konfliktlosningsmodell_htm_files/509.jpg",
+ thumbnail: "konfliktlosningsmodell_htm_files/521.jpg"
+}
+];
