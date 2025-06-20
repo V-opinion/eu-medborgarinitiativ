0fb53542fd7e208e8653c1b7836226d6697cc478
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-06-20 23:41:02 +0200

    Add files via upload

---
+++ om-MultiReligionVal-global/loadGTM.js
@@ -0,0 +1,13 @@
+(function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-L2VMFHD9ZB");
+
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-L2VMFHD9ZB";
+    document.body.appendChild(script);
+})();
