5aed0641b5d2116d95be2e86d85f0a50f9ba5e5b
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-07-03 22:42:10 +0200

    Update 1.js

--- svenska-kyrkans-privilegier/1.js
+++ svenska-kyrkans-privilegier/1.js
@@ -7,38 +7,38 @@ window.local_cyclemec_0_data = [
  trans: "0.8"
 },
 {
- bimage: "svenska-kyrkans-privilegier_htm_files/562.png",
- desc: "svenska-kyrkans-privilegier_htm_files/563.png",
- heading: "svenska-kyrkans-privilegier_htm_files/564.png",
- image: "svenska-kyrkans-privilegier_htm_files/565.png",
+ bimage: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/562.png",
+ desc: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/563.png",
+ heading: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/564.png",
+ image: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/565.png",
  link: "#"
 },
 {
- bimage: "svenska-kyrkans-privilegier_htm_files/562.png",
- desc: "svenska-kyrkans-privilegier_htm_files/566.png",
- heading: "svenska-kyrkans-privilegier_htm_files/567.png",
- image: "svenska-kyrkans-privilegier_htm_files/550.jpg",
+ bimage: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/562.png",
+ desc: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/566.png",
+ heading: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/567.png",
+ image: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/550.jpg",
  link: "#"
 },
 {
- bimage: "svenska-kyrkans-privilegier_htm_files/562.png",
- desc: "svenska-kyrkans-privilegier_htm_files/568.png",
- heading: "svenska-kyrkans-privilegier_htm_files/569.png",
- image: "svenska-kyrkans-privilegier_htm_files/570.png",
+ bimage: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/562.png",
+ desc: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/568.png",
+ heading: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/569.png",
+ image: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/570.png",
  link: "#"
 },
 {
- bimage: "svenska-kyrkans-privilegier_htm_files/562.png",
- desc: "svenska-kyrkans-privilegier_htm_files/571.png",
- heading: "svenska-kyrkans-privilegier_htm_files/572.png",
- image: "svenska-kyrkans-privilegier_htm_files/573.jpg",
+ bimage: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/562.png",
+ desc: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/571.png",
+ heading: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/572.png",
+ image: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/573.jpg",
  link: "#"
 },
 {
- bimage: "svenska-kyrkans-privilegier_htm_files/562.png",
- desc: "svenska-kyrkans-privilegier_htm_files/574.png",
- heading: "svenska-kyrkans-privilegier_htm_files/575.png",
- image: "svenska-kyrkans-privilegier_htm_files/576.jpg",
+ bimage: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/562.png",
+ desc: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/574.png",
+ heading: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/575.png",
+ image: "https://multireligionvalsystem.eu.org/svenska-kyrkans-privilegier/576.jpg",
  link: "#"
 }
 ];
