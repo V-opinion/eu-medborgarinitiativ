304c6c6caf3ccee84a3d8039212bc39d50f559ec
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-07-12 01:31:42 +0200

    Add files via upload

---
+++ unikt-konfliktlösningsmodellskoncept/global.js
@@ -0,0 +1,7 @@
+// Library Imports
+import '/vendor/bootstrap/bootstrap.bundle.min.js';
+import '/vendor/fontawesome/all.min.js';
+
+// Custom Imports
+import './dark-mode.js';
+import './copyright.js';
