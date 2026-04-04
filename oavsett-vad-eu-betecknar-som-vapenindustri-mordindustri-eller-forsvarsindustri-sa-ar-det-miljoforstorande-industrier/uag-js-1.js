1224b55975025cbe4c5a2b898293b74530993c54
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-04-05 01:04:36 +0200

    Add files via upload

---
+++ oavsett-vad-eu-betecknar-som-vapenindustri-mordindustri-eller-forsvarsindustri-sa-ar-det-miljoforstorande-industrier/uag-js-1.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9d7ceef2' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d5902149' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-3a9acef5' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-2fdfd925' );
+});
+ });
