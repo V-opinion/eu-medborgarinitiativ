141f9a005cd93bed5accfa23ee972a8f13856a7a
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-04-20 02:16:24 +0200

    Add files via upload

---
+++ miljömodellen-för-demokratin-demokratin-från-terori-till-praktik-Multireligionvalsystem/demokrati-i-religionerna/webcontext.js
@@ -0,0 +1,10 @@
+var webContext = {
+  "webId": 1730781,
+  "unitId": 2147483057,
+  "projectId": 2147483057,
+  "type": "project",
+  "webName": "Multireligionval",
+  "local": true,
+  "localization": null,
+  "samfCode": null
+}
