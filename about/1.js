311fab1e4c4596960896cddf44b00209d6261145
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-04-25 20:10:26 +0200

    Update image URLs in local_nivoslider_1_data

--- about/1.js
+++ about/1.js
@@ -5,11 +5,11 @@ window.local_nivoslider_1_data = [
 },
 {
  caption: "Multireligionvalsystem.",
- image: "FDU_htm_files/1409.png"
+ image: "https://multireligionvalsystem.eu.org/about/1409.png"
 },
 {
  caption: "political practices in exile ",
- image: "FDU_htm_files/1410.png"
+ image: "https://multireligionvalsystem.eu.org/about/1410.png"
 },
 {
  caption: "F\u00F6r en h\u00E5llbar politik som leder  till en h\u00E5llbar utveckling. ",
@@ -17,6 +17,6 @@ window.local_nivoslider_1_data = [
 },
 {
  caption: "Den 77 \u00E5r l\u00E5nga konflikten mellan Israel och Palestina\u000A.",
- image: "FDU_htm_files/1412.png"
+ image: "https://multireligionvalsystem.eu.org/about/1412.png"
 }
 ];
