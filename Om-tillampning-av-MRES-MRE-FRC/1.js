ab41817e7e963529ed2d65a048dc187ae04b3339
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-04-25 17:09:17 +0200

    Change local image paths to absolute URLs
    Updated image URLs to use absolute paths.

--- Om-tillampning-av-MRES-MRE-FRC/1.js
+++ Om-tillampning-av-MRES-MRE-FRC/1.js
@@ -1,27 +1,27 @@
 window.local_cycleBottomradio_v8_6_data = [
 {
  bulletimage: "Om-tillampning-av-MRES-MRE-FRC_htm_files/472.png",
- image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/473.jpg",
+ image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/473.jpg",
  link: "#"
 },
 {
- image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/474.png",
+ image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/474.png",
  link: "#"
 },
 {
- image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/475.jpg",
+ image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/475.jpg",
  link: "#"
 },
 {
- image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/476.jpg",
+ image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/476.jpg",
  link: "#"
 },
 {
- image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/477.jpg",
+ image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/477.jpg",
  link: "#"
 },
 {
- image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/478.jpg",
+ image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/478.jpg",
  link: "#"
 }
 ];
