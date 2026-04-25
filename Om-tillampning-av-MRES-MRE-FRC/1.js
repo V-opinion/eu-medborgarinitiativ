9b9821796591bfd415f8b120710ed40be7155aad
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-04-25 16:58:23 +0200

    Add files via upload

---
+++ Om-tillampning-av-MRES-MRE-FRC/1.js
@@ -0,0 +1,27 @@
+window.local_cycleBottomradio_v8_6_data = [
+{
+ bulletimage: "Om-tillampning-av-MRES-MRE-FRC_htm_files/472.png",
+ image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/473.jpg",
+ link: "#"
+},
+{
+ image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/474.png",
+ link: "#"
+},
+{
+ image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/475.jpg",
+ link: "#"
+},
+{
+ image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/476.jpg",
+ link: "#"
+},
+{
+ image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/477.jpg",
+ link: "#"
+},
+{
+ image: "Om-tillampning-av-MRES-MRE-FRC_htm_files/478.jpg",
+ link: "#"
+}
+];
