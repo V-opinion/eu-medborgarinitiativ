b8d6abca0881b652f2e9556f3b0063917f659ee5
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-05-22 16:36:52 +0200

    Update import URLs to use 'www' subdomain

--- unikt-konfliktlösningsmodellskoncept/global.js
+++ unikt-konfliktlösningsmodellskoncept/global.js
@@ -1,7 +1,7 @@
 // Library Imports
-import 'https://multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/bootstrap.bundle.min.js';
-import 'https://multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/fontawesome/all.min.js';
+import 'https://www.multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/bootstrap.bundle.min.js';
+import 'https://www.multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/fontawesome/all.min.js';
 
 // Custom Imports
-import 'https://multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/dark-mode.js';
-import 'https://multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/copyright.js';
+import 'https://www.multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/dark-mode.js';
+import 'https://www.multireligionvalsystem.eu.org/unikt konfliktlösningsmodellskoncept/copyright.js';
