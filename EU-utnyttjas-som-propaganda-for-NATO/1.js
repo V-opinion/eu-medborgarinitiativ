e3de41a283845e43f74b38319ee6bed46422041d
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-05-23 16:16:01 +0200

    Update 1.js

--- EU-utnyttjas-som-propaganda-for-NATO/1.js
+++ EU-utnyttjas-som-propaganda-for-NATO/1.js
@@ -4,39 +4,39 @@ window.local_simpleGallery3Columns_0_data = [
  showshadow: "Y"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/420.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/421.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/420.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/421.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/422.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/423.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/422.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/423.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/424.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/425.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/424.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/425.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/426.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/427.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/426.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/427.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/428.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/429.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/428.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/429.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/430.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/431.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/430.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/431.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/432.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/433.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/432.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/433.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/434.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/435.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/434.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/435.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/436.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/437.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/436.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/EU-utnyttjas-som-propaganda-for-NATO/437.jpg"
 }
 ];
