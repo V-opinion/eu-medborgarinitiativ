7993c0fa4b6244d75f4614577e55e84091220dda
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-05-23 16:43:51 +0200

    Update 1.js

--- Enskild-motion-FDU-Riksdagen/1.js
+++ Enskild-motion-FDU-Riksdagen/1.js
@@ -5,18 +5,18 @@ window.local_nivoslider_0_data = [
 },
 {
  caption: "Den gr\u00F6na omst\u00E4llningen",
- image: "https://multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/19.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/19.jpg"
 },
 {
  caption: "H\u00E5llbar utveckling.",
- image: "https://multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/55.png"
+ image: "https://www.multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/55.png"
 },
 {
  caption: "Den svenska deokratin..",
- image: "https://multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/56.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/56.jpg"
 },
 {
  caption: "Multireligiovalsystem",
- image: "https://multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/57.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/Enskild-motion-FDU-Riksdagen/57.jpg"
 }
 ];
