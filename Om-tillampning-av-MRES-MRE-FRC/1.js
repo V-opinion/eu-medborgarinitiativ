f3f8916360d9404e4d0d92e980ed739e00eb68e4
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-05-24 23:49:16 +0200

    Update 1.js

--- Om-tillampning-av-MRES-MRE-FRC/1.js
+++ Om-tillampning-av-MRES-MRE-FRC/1.js
@@ -1,27 +1,27 @@
 window.local_cycleBottomradio_v8_6_data = [
 {
  bulletimage: "Om-tillampning-av-MRES-MRE-FRC_htm_files/472.png",
- image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/473.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/473.jpg",
  link: "#"
 },
 {
- image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/474.png",
+ image: "https://www.multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/474.png",
  link: "#"
 },
 {
- image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/475.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/475.jpg",
  link: "#"
 },
 {
- image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/476.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/476.jpg",
  link: "#"
 },
 {
- image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/477.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/477.jpg",
  link: "#"
 },
 {
- image: "https://multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/478.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/Om-tillampning-av-MRES-MRE-FRC/478.jpg",
  link: "#"
 }
 ];
