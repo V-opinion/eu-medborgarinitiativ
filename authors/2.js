4fc65d2a42b816daa9bd723ed5df8a64b1066fd2
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-05-25 15:36:47 +0200

    Update 2.js

--- authors/2.js
+++ authors/2.js
@@ -4,39 +4,39 @@ window.local_simpleGallery3Columns_1_data = [
  showshadow: "Y"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/71.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/490.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/71.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/490.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/473.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/491.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/473.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/491.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/475.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/492.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/475.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/492.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/477.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/493.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/477.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/493.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/479.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/494.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/479.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/494.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/481.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/495.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/481.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/495.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/483.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/496.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/483.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/496.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/485.jpg",
- thumbnail: https://multireligionvalsystem.eu.org/authors//497.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/485.jpg",
+ thumbnail: https://www.multireligionvalsystem.eu.org/authors//497.jpg"
 },
 {
- image: "https://multireligionvalsystem.eu.org/authors/487.jpg",
- thumbnail: "https://multireligionvalsystem.eu.org/authors/498.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/authors/487.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/authors/498.jpg"
 }
 ];
