a9a601a41bacd82310b2b96ad63c0fb0cf629b28
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-05-28 22:39:52 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,27 +1,27 @@
 window.local_cycleBottomradio_v8_6_data = [
 {
- bulletimage: "fns%20rollspel_htm_files/472.png",
- image: "fns%20rollspel_htm_files/473.jpg",
+ bulletimage: "https://www.multireligionvalsystem.eu.org/472.png",
+ image: "https://www.multireligionvalsystem.eu.org/473.jpg",
  link: "#"
 },
 {
- image: "fns%20rollspel_htm_files/474.png",
+ image: "https://www.multireligionvalsystem.eu.org/474.png",
  link: "#"
 },
 {
- image: "fns%20rollspel_htm_files/475.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/475.jpg",
  link: "#"
 },
 {
- image: "fns%20rollspel_htm_files/476.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/476.jpg",
  link: "#"
 },
 {
- image: "fns%20rollspel_htm_files/477.jpg",
+ image: https://www.multireligionvalsystem.eu.org/477.jpg",
  link: "#"
 },
 {
- image: "fns%20rollspel_htm_files/478.jpg",
+ image: "https://www.multireligionvalsystem.eu.org/478.jpg",
  link: "#"
 }
 ];
