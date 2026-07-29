c7751ed7747e61e912bf2fb7efa7a49cf9bcd4fc
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2026-07-29 15:09:29 +0200

    Change image links to HTTPS in 1.js
    Updated image URLs to use HTTPS for better security.

--- kyrkovalsreformen/kyrkovalsreformen/1.js
+++ kyrkovalsreformen/kyrkovalsreformen/1.js
@@ -4,39 +4,39 @@ window.local_simpleGallery3Columns_0_data = [
  showshadow: "Y"
 },
 {
- image: "konfliktlosningsmodell_htm_files/493.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/494.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/493.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/494.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/495.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/496.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/495.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/496.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/497.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/498.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/497.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/498.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/499.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/500.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/499.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/500.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/501.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/502.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/501.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/502.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/503.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/504.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/503.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/504.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/505.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/506.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/505.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/506.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/507.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/508.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/507.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/508.jpg"
 },
 {
- image: "konfliktlosningsmodell_htm_files/509.jpg",
- thumbnail: "konfliktlosningsmodell_htm_files/510.jpg"
+ image: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/509.jpg",
+ thumbnail: "https://www.multireligionvalsystem.eu.org/kyrkovalsreformen/kyrkovalsreformen/510.jpg"
 }
 ];
